3c98b27023ecc8ca83504dbd0140ff0e36956bdc
Felipe Fonseca <feli13pe@gmail.com>
2024-08-19 17:43:11 -0500

    primer dia de codigo

---
+++ day1.py
@@ -0,0 +1,35 @@
+"""Print statements practice"""
+
+print("Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+
+print("Knead the dough for 10 minutes.")
+
+print("Add 3g of Salt.")
+
+print("Leave to rise for 2 hours.")
+
+print("Bake at 200 degrees C for 30 minutes.")
+
+# imprimir varios strings con solo una funcion print
+
+print("Hello world\nHello world")
+
+# concatenar strings
+
+name = "mono"
+
+print("Hello", "Feldos")
+
+print(f"Hello {name}")
+
+# input function
+
+print("Hello "+ input("what is your name ?: ")+ "!")
+print("Hello", input("What is your pokemon? : ")+"!")
+
+
+# function len()
+
+namedos = input("Enter your name please: ")
+length_name = len(namedos)
+print(length_name)
