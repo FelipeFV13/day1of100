5ab2778e5318108f31d0b8fbabe64d95e2c4d0da
Felipe Fonseca <feli13pe@gmail.com>
2024-08-19 18:04:33 -0500

    primer proyecto generador nombres de bandas

---
+++ band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+name_of_city = input("Whitch city did you grow up in? \n")
+name_of_pets = input("What's your pet's name?\n")
+print("the band name could be",name_of_city, name_of_pets)
